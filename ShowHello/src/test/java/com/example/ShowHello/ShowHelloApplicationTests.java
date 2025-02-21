df3ad98edebc7d0a9077d1db2d8312b1da86d5ef
Riya <soniriya2409@gmail.com>
2025-02-21 13:55:50 +0530

    [Riya] Add. Hello From Bridgelabz

---
+++ ShowHello/src/test/java/com/example/ShowHello/ShowHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShowHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
