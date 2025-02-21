fbb06253bf66d4720fe90c3eeffa4fb7ffc42394
Riya <soniriya2409@gmail.com>
2025-02-21 14:46:58 +0530

    [Riya] Add. Show Mark

--- ShowHello/src/main/java/com/example/ShowHello/HelloController.java
+++ ShowHello/src/main/java/com/example/ShowHello/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.ShowHello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello from bridgelabz";
+    }
+    @GetMapping("/hello/query")
+    public String greetUser(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
