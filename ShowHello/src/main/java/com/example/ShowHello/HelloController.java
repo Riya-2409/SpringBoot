40ca4481b4ebc37043f42e658a4e082c3483acfb
Riya <soniriya2409@gmail.com>
2025-02-21 16:00:35 +0530

    [Riya] Add. Show Mark Taylor

--- ShowHello/src/main/java/com/example/ShowHello/HelloController.java
+++ ShowHello/src/main/java/com/example/ShowHello/HelloController.java
@@ -1,11 +1,9 @@
 package com.example.ShowHello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
@@ -15,10 +13,13 @@ public class HelloController {
     public String greetUser(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
     }
-    // Path Variable Example: /hello/param/Mark
     @GetMapping("hello/param/{name}")
     public String sayHelloWithPathVariable(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+    @PostMapping("/post")
+    public String sayHello(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 
 }
